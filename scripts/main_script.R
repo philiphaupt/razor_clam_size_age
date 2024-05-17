1fa581650507b5dd0b06ea69d2a151e4684978da
Philip Haupt <philip.haupt@kentandessex-ifca.gov.uk>
2024-05-17 15:12:19 +0100

    change file.edit to source

--- scripts/main_script.R
+++ scripts/main_script.R
@@ -30,10 +30,10 @@ source("./scripts/biomass_year_class_calculations.R", echo = TRUE)
 source("scripts/read_spatial_data.R", echo = FALSE)
 
 # join survey results to spatial data
-file.edit("./scripts/join_survey_to_spatial.R")
+source("./scripts/join_survey_to_spatial.R", echo =TRUE)
 
 # Shell dimensions shell HEIGHT vs shell length
-file.edit("./scripts/shell_length_v_shell_height.R", echo = TRUE)
+source("./scripts/shell_length_v_shell_height.R", echo = TRUE)
 
 # Shell dimensions shell WIDTH (across bivalves) vs shell length
-file.edit("./scripts/shell_length_v_shell_width.R", echo = TRUE)
+source("./scripts/shell_length_v_shell_width.R", echo = TRUE)
